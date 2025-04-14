2eadad9fec1f4652225e450ca6186368026d9fd4
ArdillaV21 <fedevalle21@gmail.com>
2025-04-14 18:51:36 -0300

    Generando datos aleatorios de 4b

--- tp2_4.c
+++ tp2_4.c
@@ -1,4 +1,7 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#define CANT_PC 5
 
 // Definición de la estructura para representar una computadora
 struct compu {
@@ -7,6 +10,19 @@ struct compu {
     int cantidad_nucleos; // Cantidad de núcleos (1–8)
     char *tipo_cpu;        // Tipo de procesador (puntero a cadena de caracteres)
 };
+
+void generarDatos(struct compu pcs[], int cantidad) {
+    char tipos[6][10] = {"Intel", "AMD", "Celeron", "Athlon", "Core", "Pentium"};
+
+    srand(time(NULL));
+
+    for (int i = 0; i < cantidad; i++) {
+        pcs[i].velocidad = 1 + rand() % 3;         
+        pcs[i].anio = 2015 + rand() % 10;             
+        pcs[i].cantidad_nucleos = 1 + rand() % 8;       
+        pcs[i].tipo_cpu = tipos[rand() % 6];            
+    }
+}
 
 int main(){
 
