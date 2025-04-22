7c76c3cb8e343b11b6fbf8df9aecd24c285670d4
Federico Valle <138173670+ArdillaV21@users.noreply.github.com>
2025-04-22 13:45:30 -0300

    add: "Agregue la funcion pcMasVieja y pcMasVeloz"

--- tp2_4.c
+++ tp2_4.c
@@ -1,46 +1,73 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <time.h>
-#define CANT_PC 5
 
-// Definición de la estructura para representar una computadora
-struct compu {
-    int velocidad;         // Velocidad de procesamiento en GHz (1–3)
-    int anio;              // Año de fabricación (2015–2024)
-    int cantidad_nucleos; // Cantidad de núcleos (1–8)
-    char *tipo_cpu;        // Tipo de procesador (puntero a cadena de caracteres)
+typedef struct compu{
+    int velocidad;
+    int anio;
+    int cantidad_nucleos;
+    char *tipo_cpu;
+}compu;
+
+void generarPCs(struct compu pcs[],int cantidad){
+    
+    char *tipos[] = {"Intel","AMD","Celereon","Athlon","Core","Pentium"};
+    srand(time(NULL));
+    for(int i=0;i<cantidad;i++){
+        pcs[i].velocidad = 1 + rand() % 3;
+        pcs[i].anio = 2015 + rand() % 10;
+        pcs[i].cantidad_nucleos = 1 + rand() % 8;
+        pcs[i].tipo_cpu = tipos[rand() % 6];
+    }
+}
+
+void listarPcs(struct compu pcs[],int cantidad){
+    for(int i=0;i<cantidad;i++){
+        printf("\nVelocidad = %d \n", pcs[i].velocidad);
+        printf("Año = %d \n", pcs[i].anio);
+        printf("Cantidad Nucleos = %d \n", pcs[i].cantidad_nucleos);
+        printf("Tipo de CPU = %s \n", pcs[i].tipo_cpu);
+        printf("------------------------------------------------- \n");
+    }
 };
 
-void generarDatos(struct compu pcs[], int cantidad) {
-    char tipos[6][10] = {"Intel", "AMD", "Celeron", "Athlon", "Core", "Pentium"};
+void mostrarMasVieja(struct compu pcs[], int cantidad){
+    int aux = pcs[0].anio;
+    int valor;
+    for(int i=0; i<cantidad; i++){
+        if(pcs[i].anio < aux){
+            valor = i;
+        };
+    };
+    printf("----------------PC MAS VIEJA------------------- \n");
+    printf("\nVelocidad = %d \n", pcs[valor].velocidad);
+    printf("Año = %d \n", pcs[valor].anio);
+    printf("Cantidad Nucleos = %d \n", pcs[valor].cantidad_nucleos);
+    printf("Tipo de CPU = %s \n", pcs[valor].tipo_cpu);
+};
 
-    srand(time(NULL));
-
-    for (int i = 0; i < cantidad; i++) {
-        pcs[i].velocidad = 1 + rand() % 3;         
-        pcs[i].anio = 2015 + rand() % 10;             
-        pcs[i].cantidad_nucleos = 1 + rand() % 8;       
-        pcs[i].tipo_cpu = tipos[rand() % 6];            
-    }
-}
-
-void listarPCs(struct compu pcs[], int cantidad) {
-    printf("Lista de PCs:\n");
-    for (int i = 0; i < cantidad; i++) {
-        printf("PC: %d:\n", i + 1);
-        printf("  Velocidad: %d GHz\n", pcs[i].velocidad);
-        printf("  Año: %d\n", pcs[i].anio);
-        printf("  Núcleos: %d\n", pcs[i].cantidad_nucleos);
-        printf("  Tipo de CPU: %s\n", pcs[i].tipo_cpu);
-    }
-}
+void mostrarMasVeloz(struct compu pcs[], int cantidad){
+    int aux1 = pcs[0].velocidad;
+    int valor1;
+    for(int i=0; i<cantidad; i++){
+        if(pcs[i].velocidad > aux1){
+            valor1 = i;
+        };
+    };
+    printf("----------------PC MAS RAPIDA------------------- \n");
+    printf("\nVelocidad = %d \n", pcs[valor1].velocidad);
+    printf("Año = %d \n", pcs[valor1].anio);
+    printf("Cantidad Nucleos = %d \n", pcs[valor1].cantidad_nucleos);
+    printf("Tipo de CPU = %s \n", pcs[valor1].tipo_cpu);
+    printf("------------------------------------------------- \n");
+};
 
 int main(){
-    struct compu pcs[CANT_PC];
-    
-    generarDatos(pcs, CANT_PC);
-    listarPCs(pcs, CANT_PC);
-    
+    struct compu pcs[5];
+
+    generarPCs(pcs,5);
+    listarPcs(pcs,5);
+    mostrarMasVieja(pcs,5);
+    mostrarMasVeloz(pcs,5);
 
     return 0;
 }
